4a1af9002ba6446397f6c33bbeb7c192931cb324
Sponi92 <sven.sponagel@freenet.de>
2020-12-25 12:10:27 +0100

    cleaned Code

--- etch.js
+++ etch.js
@@ -1,3 +1,60 @@
+makeGrid(16);
+addClass(changeBackgroundBlack);
+document.querySelector("#btn").addEventListener("click", clearandNew);
+document.querySelector("#btn1").addEventListener("click", clearandRainbow);
+document.querySelector("#btn2").addEventListener("click", clearandGradient);
+
+function clearandNew(){
+	clear();
+	addClass(changeBackgroundBlack);	
+}
+
+function clearandRainbow(){
+	clear();
+	addClass(changeBackgroundRainbow);	
+}
+
+function clearandGradient(){
+	clear();
+	i=0;
+	addClass(changeBackgroundGradient);	
+}
+
+function clear(){
+	const clear=document.querySelectorAll(".grid-item");
+	for (i=0; i<clear.length; i++){
+		clear[i].remove();
+}
+	input=window.prompt("How many Squares per side do you want?");
+	if (input>100 || isNaN(input)){
+		alert("ERROR!");
+	}
+	else {
+		makeGrid(input);
+	}
+}
+function addClass(param1){
+	const hoverover= document.querySelectorAll(".grid-item");
+	hoverover.forEach(hoverover => hoverover.addEventListener ("mouseover",function() {param1(hoverover)}));
+}
+
+function changeBackgroundBlack(x){
+	x.classList.add("hover");
+}
+function changeBackgroundRainbow(x){
+	let randomColor = Math.floor(Math.random()*16777215).toString(16);
+	x.style.setProperty("--color", randomColor);
+	x.classList.add("rainbow");
+}
+function changeBackgroundGradient(x){
+	let gradientBlack;
+	gradientBlack=(1+i)*0.1; 
+	x.style.setProperty("--gradientBlack", gradientBlack);
+	x.classList.add("gradient");
+	i++;
+	return i;
+}
+
 function makeGrid(input) {
 	const container= document.querySelector("#container");
 	container.style.setProperty('--rows', input);
@@ -7,100 +64,5 @@ function makeGrid(input) {
     		cell.innerText = " ";
     		container.appendChild(cell).className = "grid-item";
     		
-}
-}
-makeGrid(16);
-black();
-
-function black(){
-const hoverover= document.querySelectorAll(".grid-item");
-hoverover.forEach(hoverover => hoverover.addEventListener ("mouseover",function() {changeBackground(hoverover)}));
-
-function changeBackground(x){
-x.classList.add("hover");
-
-};
-}
-
-function rainbow(){
-const hoverover= document.querySelectorAll(".grid-item");
-hoverover.forEach(hoverover => hoverover.addEventListener ("mouseover",function() {changeBackground(hoverover)}));
-
-function changeBackground(x){
-let randomColor = Math.floor(Math.random()*16777215).toString(16);
-x.style.setProperty("--color", randomColor);
-x.classList.add("rainbow");
-
-};
-}
-
-function gradient(){
-i=0;
-const hoverover= document.querySelectorAll(".grid-item");
-hoverover.forEach(hoverover => hoverover.addEventListener ("mouseover",function() {changeBackground(hoverover)}));
-
-function changeBackground(x){
-let gradientBlack;
-gradientBlack=(1+i)*0.1; 
-x.style.setProperty("--gradientBlack", gradientBlack);
-x.classList.add("gradient");
-i++;
-return i;
-};
-}
-
-
-
-document.querySelector("#btn").addEventListener("click", clearandNew);
-
-document.querySelector("#btn1").addEventListener("click", clearandRainbow);
-
-document.querySelector("#btn2").addEventListener("click", clearandGradient);
-
-function clearandNew(){
-	const clear=document.querySelectorAll(".grid-item");
-	for (i=0; i<clear.length; i++){
-		clear[i].remove();
-}
-	input=window.prompt("How many Squares per side do you want?");
-	if (input>100 || isNaN(input)){
-	alert("ERROR!");
 	}
-	else {
-	makeGrid(input);
-	}
-	black();
- 	
-}
-
-function clearandRainbow(){
-	const clear=document.querySelectorAll(".grid-item");
-	for (i=0; i<clear.length; i++){
-		clear[i].remove();
-}
-	input=window.prompt("How many Squares per side do you want?");
-	if (input>100 || isNaN(input)){
-	alert("ERROR!");
-	}
-	else {
-	makeGrid(input);
-	}
-	rainbow();
- 	
-}
-
-function clearandGradient(){
-	const clear=document.querySelectorAll(".grid-item");
-	for (i=0; i<clear.length; i++){
-		clear[i].remove();
-}
-	input=window.prompt("How many Squares per side do you want?");
-	if (input>100 || isNaN(input)){
-	alert("ERROR!");
-	}
-	else {
-	makeGrid(input);
-	}
-	gradient();
- 	
 }
